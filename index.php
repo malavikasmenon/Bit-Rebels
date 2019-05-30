e4a95ec2e0293e9645e585b62f22c97a2fee6dea
malavikasmenon <42709307+malavikasmenon@users.noreply.github.com>
2019-05-30 10:19:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+
+<html>
+ <head>
+   <title> J.K. Rowling </title>
+   <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+   <style type="text/css">
+   
+
+  
+		
+		 
+		
+		    
+	     #grey{ 
+		   background-color:#ebebe0;
+	       padding:20px;
+		   border-radius:1em;
+		   width:25%;
+		   margin-left:40px;
+		   font-family: 'Josefin Sans', sans-serif;
+         }
+		  h1{
+		   text-align:center;
+		   font-family: 'Abril Fatface', cursive;
+		
+		 }
+		  #im{ 
+		  margin:50px 180px;
+		  border-color:#ebebe0;
+		  border:5px;
+		  border-radius:20%
+		 }
+   
+   </style>
+ </head>
+
+
+ <body>
+      <h1> J.K. Rowling - The Queen </h1>
+	  <div id="im">
+     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/J._K._Rowling_2010.jpg/330px-J._K._Rowling_2010.jpg" align="center">
+	</div>
+	<p>Joanne Rowling, a British author, has authored several books and is famous for her work of fiction, The <strong>Harry Potter </strong> series.</o>
+	<div id="grey">
+   <p><ins> Works </ins>
+      <ol>
+	   <li> Harry Potter and the Philosopher's Stone </li>
+	   <li> Harry Potter and the Chamber of Secrets </li>
+	   <li> Harry Potter and the Prisoner of Azkaban </li>
+	   <li> Fantastic Beasts and Where to Find Them </li>
+	   <li> Cuckoo's Calling </li>
+	 </div>
+ </body>
+ </html>
+ 
+ 
